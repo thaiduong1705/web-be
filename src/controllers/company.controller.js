702b57cfcde6b7feffb05c34263db41a58ec9fb8
Thai Duong <thaiduong7v@gmail.com>
2023-04-15 22:39:05 +0700

    thêm công ty, xem chi tiết 1 công ty

--- src/controllers/company.controller.js
+++ src/controllers/company.controller.js
@@ -1,6 +1,6 @@
 const connection = require("../config/db.connect");
 class CompanyController {
-    // [GET]: /company/
+    // [GET]: /company/get-all-company
     getAllCompany(req, res, next) {
         connection.execute("SELECT * FROM CONGTY", (err, rows, fields) => {
             if (err) {
@@ -8,7 +8,52 @@ class CompanyController {
                 return;
             }
             res.status(200).json(rows);
-        })
+        });
+    }
+
+    // [GET]: /company/get-company/:id
+    getCompany(req, res, next) {
+        connection.execute(`SELECT * FROM CONGTY WHERE MaCongTy=${req.params.id}`, (err, rows, fields) => {
+            if (err) {
+                res.status(500).send(err);
+                return;
+            }
+            res.status(200).json(rows);
+        });
+    }
+
+    // [POST]: /company/create-new-company
+    createCompany(req, res, next) {
+        const columnNames = [
+            "TenCongTy",
+            "Website",
+            "DiaChiChinh",
+            "GioiThieuCongTy",
+            "SoLuongNhanVienMin",
+            "SoLuongNhanVienMax",
+        ];
+        let missing = [];
+        columnNames.forEach((name) => {
+            if (!req.body.hasOwnProperty(name)) {
+                missing.push(name);
+            }
+        });
+        if (missing.length != 0) {
+            res.status(400).json("Missing: " + missing);
+            return;
+        }
+        const values = columnNames.map((name) => req.body[name]);
+        connection.execute(
+            `INSERT INTO congty (TenCongTy, Website, DiaChiChinh, GioiThieuCongTy, SoLuongNhanVienMin, SoLuongNhanVienMax) VALUES (?, ?, ?, ?, ?, ?);`,
+            values,
+            (err, result) => {
+                if (err) {
+                    res.status(500).json({ err, values: values });
+                    return;
+                }
+                res.status(201).json("Number of records inserted: " + result.affectedRows);
+            },
+        );
     }
 }
 
--- src/routes/apis/company.router.js
+++ src/routes/apis/company.router.js
@@ -4,5 +4,7 @@ const router = express.Router();
 const companyController = require("../../controllers/company.controller");
 
 router.get("/get-all-company", companyController.getAllCompany);
+router.get("/get-company/:id", companyController.getCompany);
 
+router.post("/create-new-company", companyController.createCompany);
 module.exports = router;
