0a4e4cd5e4d55278da60ca36a90e6f69299f9bd6
Thai Duong <thaiduong7v@gmail.com>
2023-04-16 22:46:47 +0700

    update company, search company by name

--- src/routes/apis/company.router.js
+++ src/routes/apis/company.router.js
@@ -4,7 +4,10 @@ const router = express.Router();
 const companyController = require("../../controllers/company.controller");
 
 router.get("/get-all-company", companyController.getAllCompany);
-router.get("/get-company/:id", companyController.getCompany);
+router.get("/get-company/:id", companyController.getCompanyById);
+router.get("/get-companies", companyController.getCompaniesByName);
 
 router.post("/create-new-company", companyController.createCompany);
+
+router.put("/update-company/:id", companyController.updateCompany);
 module.exports = router;
