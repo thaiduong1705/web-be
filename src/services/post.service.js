b2174605d952bc501fb491a1c065f22d8efd9065
quangbruh123 <20520716@gm.uit.edu.vn>
2023-06-25 13:45:47 +0700

    Sửa API post limit

--- src/services/post.service.js
+++ src/services/post.service.js
@@ -248,7 +248,7 @@ export const getLimitPostsService = async ({ page, limit, order = "desc", ...que
         queries.offset = offset * numberOfItems;
         queries.limit = numberOfItems;
 
-        if (order) queries.order = ["createdAt", order];
+        if (order) queries.order = [["createdAt", order]];
 
         if (query.jobTitle) filter.jobTitle = { [Op.substring]: query.jobTitle };
         if (query.age && query.age.length !== 0) {
